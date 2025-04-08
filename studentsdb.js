e9c6e19ac301d6229ca99324b497056b24e04e12
Nipunika Nawarathne <nipunika1024@gmail.com>
2025-04-08 15:48:00 +0530

    Add files via upload

---
+++ studentsdb.js
@@ -0,0 +1,10 @@
+//array of student JSON details
+let students = [
+    {regno:'2021ICT10', name:'Harshan', age:'24', course:'IT', gender: 'Male'},
+    {regno:'2021ICT41', name:'Nipunika', age:'25', course:'IT', gender: 'Female'},
+    {regno:'2021ICT178', name:'Malani', age:'56', course:'BST', gender: 'Female'},
+    {regno:'2021ICT12', name:'Jane', age:'28', course:'IT', gender: 'Female'},
+    {regno:'2021ICT56', name:'Chathura', age:'35', course:'BST', gender: 'Male'},
+    {regno:'2021ICT78', name:'Ananda', age:'59', course:'IT', gender: 'Male'}
+];
+module.exports = students; //array name
